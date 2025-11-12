c12d91c637868ab258f960f06fe5538305fbe5da
SherifTamer-10 <sherif.rezkallah@student.giu-uni.de>
2025-11-12 15:24:09 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -5,9 +5,24 @@
 
 //define enclosure class here 
 
-
-
-
+class Enclosure{
+    private:
+        string animal[];
+        int capacity;
+        int currentCount;
+    public:
+        Enclosure(string anim[],int c , int cc){
+        string animal[]=anim[];
+        int capacity=c;
+        int currentCount=cc;
+        }
+        void addAnimal(string animal[]){
+            anim[]++;
+                                    };
+        void displayAnimals(){
+            cout<<"the animals:"<<animals[]<<endl;
+                                }
+                                };
 
 
 
