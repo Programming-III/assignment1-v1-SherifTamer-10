b99acded579b2d4de848ebd18bf5ae6fbb53973d
SherifTamer-10 <sherif.rezkallah@student.giu-uni.de>
2025-11-12 15:25:43 +0200

    Update Visitor.h

--- Visitor.h
+++ Visitor.h
@@ -7,7 +7,21 @@ using namespace std;
 //define visitor class here
 
 
-
+class Visitor{
+        private:
+            string visitorName;
+            int ticketsBought;
+        public:
+            Visitor(string v, int tb){
+            visitorName=v;
+            ticketsBought=tb;
+            }
+            void displayInfo(){
+            cout<<"Visitor info:"<<endl;
+            cout<<"name:"<< visitorName<<endl;
+            cout <<"tickets bought:"<< ticketsBought <<endl;
+            }
+                        };
 
 
 
