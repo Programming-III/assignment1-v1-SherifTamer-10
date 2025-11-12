03b6d3d430521d3e041c0716448fae7bfc0dc587
SherifTamer-10 <sherif.rezkallah@student.giu-uni.de>
2025-11-12 15:21:59 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -7,5 +7,24 @@ using namespace std;
 //define animal class here 
 
 
+class Animal{
+    private:
+        string name;
+        int age;
+        bool isHungry;
+    public:
+        Animal(string n , int a ,bool isH){
+        name=n;
+        age=a;
+        isHungry=isH;
+                                            }
+        virtual void display(){
+            cout<< "name:"<<name << "age:" <<age << "isHungry:"<<isHungry<<endl;
+                    }
+        void feed(){
+            cout<< "isHungry:"<<isHungry<<endl;
+        }
+    };
+
 
 #endif
