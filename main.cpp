cf8f42888ff9b1535efccd9b7a9f126ae07475cf
SherifTamer-10 <sherif.rezkallah@student.giu-uni.de>
2025-11-12 15:26:12 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,14 @@ using namespace std;
 
 // ============== MAIN FUNCTION ==============
 int main() {
-    
+     Animal.Enclosure=new Enclosure(string anim[],int c , int cc){
+           cout<<"enclosure 1 animals:"<<endl;
+                                                                    }
+
+            Lion (Age: 5, Hungry)
+            Parrot (Age: 2, Not Hungry)
+            Snake (Age: 3, Venomous, Hungry)
+
     
     return 0;
 }
