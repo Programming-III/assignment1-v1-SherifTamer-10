f7756a08375aed1baae6d341dda17fa4db773ca6
SherifTamer-10 <sherif.rezkallah@student.giu-uni.de>
2025-11-12 15:23:29 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -5,7 +5,14 @@
 
 //define class bird here 
 
-
+        class Bird : Animal{
+        private:
+            float wingspan;
+        public:
+            Bird(float w,string n , int a ,bool isH){
+                wingspan=w;
+                }
+        };
 
 
 
